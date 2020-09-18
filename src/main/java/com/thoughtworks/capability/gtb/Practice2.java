d5f0a0e3a25c2c3ad7d3c6e80fe98bf1d7c850bc
Shen Liao <shen.liao@thoughtworks.com>
2020-09-18 17:29:23 +0800

    [Nelson Liao] feat: finish practice 2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -9,8 +9,15 @@ import java.time.LocalDate;
  * @create 2020-05-15_17:20
  */
 public class Practice2 {
+  private static final int lastWeekDay = 5;
+  private static final int nextFirstWeekDay = 7;
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    int weekDay = date.getDayOfWeek().getValue();
+    System.out.println(weekDay);
+    if(weekDay < lastWeekDay) {
+      return date.plusDays(lastWeekDay - weekDay);
+    }
+    return date.plusDays(nextFirstWeekDay + 1 - weekDay);
   }
 }
